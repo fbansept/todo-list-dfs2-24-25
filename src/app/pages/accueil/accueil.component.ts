162e233a38e8a49b1d51859b2373fae1d72ddf1c
fbansept <bansept.franck@gmail.com>
2024-11-28 15:00:20 +0100

    preparation connexion

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,16 +1,21 @@
+import { DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { RouterLink } from '@angular/router';
+import { Tache } from '../../models/Tache';
 
 @Component({
   selector: 'app-accueil',
   imports: [
     MatButtonModule,
     RouterLink,
-    MatIconModule],
+    MatIconModule,
+    MatCheckboxModule,
+    DatePipe],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.scss'
 })
@@ -18,7 +23,7 @@ export class AccueilComponent {
 
   http = inject(HttpClient)
 
-  taches: any
+  taches: Tache[] = []
 
   ngOnInit() {
     this.rafraichir()
@@ -26,13 +31,21 @@ export class AccueilComponent {
 
   rafraichir() {
     this.http
-      .get("http://localhost:3000/taches")
-      .subscribe((tachesServeur: any) => this.taches = tachesServeur)
+      .get<Tache[]>("http://localhost:3000/taches")
+      .subscribe(tachesServeur => this.taches = tachesServeur)
   }
 
   onSuppressionTache(idTacheAsupprimer: number) {
 
     this.http.delete(`http://localhost:3000/tache/${idTacheAsupprimer}`)
+      .subscribe(resultat => this.rafraichir());
+
+  }
+
+  onInversionStatus(tache: Tache) {
+    this.http.patch(
+      `http://localhost:3000/tache/change-status/${tache.id}`,
+      { fini: !tache.fini })
       .subscribe(resultat => this.rafraichir());
 
   }
--- src/app/pages/connexion/connexion.component.ts
+++ src/app/pages/connexion/connexion.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-connexion',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.scss'
 })
 export class ConnexionComponent {
+
+  
 
 }
