2101cb32f0749ada9253c6862473eb600fc26e9f
fbansept <bansept.franck@gmail.com>
2024-11-28 09:08:10 +0100

    fini suppression

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -2,11 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
-  imports: [MatButtonModule, RouterLink],
+  imports: [
+    MatButtonModule,
+    RouterLink,
+    MatIconModule],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.scss'
 })
@@ -14,13 +18,21 @@ export class AccueilComponent {
 
   http = inject(HttpClient)
 
-  taches : any
+  taches: any
 
   ngOnInit() {
 
     this.http
       .get("http://localhost:3000/taches")
       .subscribe((tachesServeur: any) => this.taches = tachesServeur)
+  }
+
+  onSuppressionTache(idTacheAsupprimer: number) {
+
+    this.http.delete(`http://localhost:3000/tache/${idTacheAsupprimer}`)
+      .subscribe(resultat => console.log(resultat));
+
+
   }
 
 }
